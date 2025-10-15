0cd4302fecca28e376072a2c992f5ceed93b4bfd
ChrisCarcamo1605 <carcamox1234@gmail.com>
2025-10-15 17:34:43 -0600

    feat: Add Supplier, Purchase, Ingredient, HistoryLog, and CashRegister entities to the database connection

--- src/compiled/infrastructure/db/Connection.js
+++ src/compiled/infrastructure/db/Connection.js
@@ -9,6 +9,11 @@ const User_1 = require("../../core/entities/User");
 const UserType_1 = require("../../core/entities/UserType");
 const Consumable_1 = require("../../core/entities/Consumable");
 const ConsumableType_1 = require("../../core/entities/ConsumableType");
+const Supplier_1 = require("../../core/entities/Supplier");
+const Purchase_1 = require("../../core/entities/Purchase");
+const Ingredient_1 = require("../../core/entities/Ingredient");
+const HistoryLog_1 = require("../../core/entities/HistoryLog");
+const CashRegister_1 = require("../../core/entities/CashRegister");
 const getDataSource = () => {
     return new typeorm_1.DataSource({
         type: "postgres",
@@ -19,7 +24,20 @@ const getDataSource = () => {
         database: "estacioncafedb",
         synchronize: true,
         logging: false,
-        entities: [Bill_1.Bill, Producto_1.Product, BillDetails_1.BillDetails, User_1.User, UserType_1.UserType, Consumable_1.Consumable, ConsumableType_1.ConsumableType],
+        entities: [
+            Bill_1.Bill,
+            Producto_1.Product,
+            BillDetails_1.BillDetails,
+            User_1.User,
+            UserType_1.UserType,
+            Consumable_1.Consumable,
+            ConsumableType_1.ConsumableType,
+            Supplier_1.Supplier,
+            Purchase_1.Purchase,
+            Ingredient_1.Ingredient,
+            HistoryLog_1.historyLog,
+            CashRegister_1.CashRegister,
+        ],
         migrations: [],
         subscribers: [],
     });
