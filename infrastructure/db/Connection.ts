5163405a3f80ec16b101c230445167887ad7e9e4
ChrisCarcamo1605 <carcamox1234@gmail.com>
2025-10-14 22:47:56 -0600

    fix: Clean up import statements and format code in Connection.ts

--- infrastructure/db/Connection.ts
+++ infrastructure/db/Connection.ts
@@ -1,14 +1,19 @@
 import { DataSource } from "typeorm";
 import { Bill } from "../../core/entities/Bill";
 import { Product } from "../../core/entities/Producto";
-import {BillDetails} from "../../core/entities/BillDetails"
+import { BillDetails } from "../../core/entities/BillDetails";
 import { User } from "../../core/entities/User";
 import { UserType } from "../../core/entities/UserType";
 import { Consumable } from "../../core/entities/Consumable";
 import { ConsumableType } from "../../core/entities/ConsumableType";
+import { Supplier } from "../../core/entities/Supplier";
+import { Purchase } from "../../core/entities/Purchase";
+import { Ingredient } from "../../core/entities/Ingredient";
+import { historyLog } from "../../core/entities/HistoryLog";
+import { CashRegister } from "../../core/entities/CashRegister";
 
-export const getDataSource = ()=>{
-    return  new DataSource({
+export const getDataSource = () => {
+  return new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5555,
@@ -17,10 +22,23 @@ export const getDataSource = ()=>{
     database: "estacioncafedb",
     synchronize: true,
     logging: false,
-    entities: [Bill,Product,BillDetails,User,UserType,Consumable,ConsumableType],
+    entities: [
+      Bill,
+      Product,
+      BillDetails,
+      User,
+      UserType,
+      Consumable,
+      ConsumableType,
+      Supplier,
+      Purchase,
+      Ingredient,
+      historyLog,
+      CashRegister,
+    ],
     migrations: [],
     subscribers: [],
   });
-}
+};
 
-module.exports = {getDataSource}
+module.exports = { getDataSource };
