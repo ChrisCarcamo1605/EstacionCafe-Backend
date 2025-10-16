08038a0ff8639e7e77c2ca2c2d3bb8b235133619
ChrisCarcamo1605 <carcamox1234@gmail.com>
2025-10-16 12:24:17 -0600

    Remove compiled files from tracking and update .gitignore

--- src/compiled/infrastructure/Routes/BillRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.billRouter = void 0;
-const express_1 = require("express");
-const BillController_1 = require("../../controller/BillController");
-exports.billRouter = (0, express_1.Router)();
-exports.billRouter.get('/bills', BillController_1.getBills);
-exports.billRouter.post('/bills', BillController_1.saveBill);
--- src/compiled/infrastructure/Routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoute = void 0;
-const express_1 = require("express");
-const UserController_1 = require("../../controller/UserController");
-exports.userRoute = (0, express_1.Router)();
-exports.userRoute.get("/users", UserController_1.getUsers);
-exports.userRoute.post("/users", UserController_1.saveUser);
